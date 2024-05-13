856335d4ca96c661cbf70d51107b3f8c337a728d
edupointktm <edupointktm@gmail.com>
2024-05-13 08:12:55 +0545

    SMS

---
+++ React/student_management_system/src/page/Home.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <section id="our-team">
+  <div className="container">
+    
+    <div className="content">At variations of passages of Lorem Ipsum available, but the majority have suffered
+      alteration..</div>
+    <div className="row">
+      <div className="col-sm-3">
+        <div className="card">
+          <div className="image"><img src="https://webthemez.com/demo/green-corp-flat-free-responsive-mobile-website/images/photo-1.jpg" alt /></div>
+          <div className="title">John Doe</div>
+          <div className="sub-title">CEO</div>
+          <div className="social-media">
+            <i className="fa-brands fa-facebook" />
+            <i className="fa fa-google-plus" />
+            <i className="fa fa-twitter" />
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3" data-aos="flip-left" data-aos-duration={500}>
+        <div className="card">
+          <div className="image"><img src="https://webthemez.com/demo/green-corp-flat-free-responsive-mobile-website/images/photo-2.jpg" alt /></div>
+          <div className="title">Larry Doe</div>
+          <div className="sub-title">Art Director</div>
+          <div className="social-media">
+            <i className="fa-brands fa-facebook" />
+            <i className="fa fa-google-plus" />
+            <i className="fa fa-twitter" />
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3" data-aos="zoom-in-up" data-aos-duration={500}>
+        <div className="card">
+          <div className="image"><img src="https://webthemez.com/demo/green-corp-flat-free-responsive-mobile-website/images/photo-3.jpg" alt /></div>
+          <div className="title">Ranith Kays</div>
+          <div className="sub-title">Manager</div>
+          <div className="social-media">
+            <i className="fa-brands fa-facebook" />
+            <i className="fa fa-google-plus" />
+            <i className="fa fa-twitter" />
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-3">
+        <div className="card">
+          <div className="image"><img src="https://webthemez.com/demo/green-corp-flat-free-responsive-mobile-website/images/photo-4.jpg" alt /></div>
+          <div className="title">Joan Ray</div>
+          <div className="sub-title">Creative</div>
+          <div className="social-media">
+            <i className="fa-brands fa-facebook" />
+            <i className="fa fa-google-plus" />
+            <i className="fa fa-twitter" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    </>
+  )
+}
+
+export default Home
---
+++ React/student_management_system/src/page/Student_details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Student_details() {
+  return (
+    <>
+    <h1> Student Details </h1>
+    </>
+  )
+}
+
+export default Student_details
